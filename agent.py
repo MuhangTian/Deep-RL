13a32ef027347162275cd68d60f000c66246d031
Tony Tian <mt361@duke.edu>
2023-04-28 22:52:31 -0400

    few changes

--- agent.py
+++ agent.py
@@ -169,29 +169,27 @@ def train(args):
     print(f"{'='*10} TRAINING FINISHED {'='*10}")
 
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--env", type=str, default="ALE/MsPacman-v5", help="gym environment")
-parser.add_argument("--mode", default="valid", choices=["train", "valid",], 
-                    help="training or validation mode")
-parser.add_argument("--total_frames", default=1000000, type=int, 
-                    help="total environment frames to train for")
-parser.add_argument("--batch_size", default=8, type=int, help="learner batch size.")
-parser.add_argument("--unroll_length", default=80, type=int, 
-                    help="unroll length (time dimension)")
-parser.add_argument("--hidden_dim", default=256, type=int, help="policy net hidden dim")
-parser.add_argument("--discounting", default=0.99, type=float, help="discounting factor")
-parser.add_argument("--learning_rate", default=0.0001, type=float, help="Learning rate")
-parser.add_argument("--grad_norm_clipping", default=10.0, type=float,
-                    help="Global gradient norm clip.")
-parser.add_argument("--save_path", type=str, default='trained/baseline_default_params.pt', help="save model here")
-parser.add_argument("--load_path", type=str, default='trained/baseline_default_params.pt', help="load model from here")
-parser.add_argument("--min_to_save", default=5, type=int, help="save every this many minutes")
-parser.add_argument("--eval_every", default=50, type=int, help="eval every this many updates")
-parser.add_argument("--render", action="store_true", default=True, help="render game-play at validation time")
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="ALE/MsPacman-v5", help="gym environment")
+    parser.add_argument("--mode", default="valid", choices=["train", "valid",], 
+                        help="training or validation mode")
+    parser.add_argument("--total_frames", default=1000000, type=int, 
+                        help="total environment frames to train for")
+    parser.add_argument("--batch_size", default=8, type=int, help="learner batch size.")
+    parser.add_argument("--unroll_length", default=80, type=int, 
+                        help="unroll length (time dimension)")
+    parser.add_argument("--hidden_dim", default=256, type=int, help="policy net hidden dim")
+    parser.add_argument("--discounting", default=0.99, type=float, help="discounting factor")
+    parser.add_argument("--learning_rate", default=0.0001, type=float, help="Learning rate")
+    parser.add_argument("--grad_norm_clipping", default=10.0, type=float,
+                        help="Global gradient norm clip.")
+    parser.add_argument("--save_path", type=str, default='trained/baseline_default_params.pt', help="save model here")
+    parser.add_argument("--load_path", type=str, default='trained/baseline_default_params.pt', help="load model from here")
+    parser.add_argument("--min_to_save", default=5, type=int, help="save every this many minutes")
+    parser.add_argument("--eval_every", default=50, type=int, help="eval every this many updates")
+    parser.add_argument("--render", action="store_true", default=True, help="render game-play at validation time")
+    
     torch.manual_seed(59006)
     np.random.seed(59006)
     args = parser.parse_args()
