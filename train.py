f8fe38c5525073683ad05a268aba5fb0acba9da8
Tony Tian <mt361@duke.edu>
2023-05-03 15:33:03 -0400

    tc

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_path", type=str, default='trained/dql.pt', help="save model here")
     parser.add_argument("--load_path", type=str, default='trained/dql.pt', help="load model from here")
     parser.add_argument("--min_to_save", default=5, type=int, help="save every this many minutes")
-    parser.add_argument("--eval_every", default=50, type=int, help="eval every this many updates")
+    parser.add_argument("--eval_every", default=2000, type=int, help="eval every this many updates")
     parser.add_argument("--render", action="store_true", default=False, help="render game-play at validation time")
     # ---------------------------------- DQN specific ----------------------------------
     parser.add_argument("--episodes", type=int, default=10000, help="episodes to train for")
