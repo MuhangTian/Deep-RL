6b7a12c0780c87ff97daf0577633537deb9c25a9
Muhang Tian <muhang.tian@duke.edu>
2023-05-14 01:03:31 -0400

    cluster: upload trained results

---
+++ cluster/ppo4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=PPO-Pong
+#SBATCH --time=10-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --partition=compsci-gpu
+#SBATCH --exclude=linux[1-40]
+#SBATCH --mem=50G
+#SBATCH --mail-user=muhang.tian@duke.edu
+#SBATCH --output=None
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate RL
+
+srun python main.py --env ALE/Pong-v5 --value_coef 1
